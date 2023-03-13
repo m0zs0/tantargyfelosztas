2fc69bf4ddaee6e1f3008764c2cd50e6ba381043
mozso <126395568+m0zs0@users.noreply.github.com>
2023-03-13 09:24:08 +0100

    2. feladat kész

---
+++ tanfel.py
@@ -0,0 +1,28 @@
+"""
+1. feladat
+Olvassa be és tárolja el a beosztas.txt állományban talált adatokat, és annak
+felhasználásával oldja meg a következő feladatokat! 
+"""
+
+beosztas={}
+beosztasok=[]
+segedlista=[]
+with open("beosztas.txt","r",encoding="utf-8") as fm1:
+    for sor in fm1:
+        segedlista.append(sor.strip())
+        if len(segedlista)==4:
+            beosztas["tanar"]=segedlista[0]
+            beosztas["tantargy"]=segedlista[1]
+            beosztas["osztaly"]=segedlista[2]
+            beosztas["oraszam"]=int(segedlista[3])
+            beosztasok.append(beosztas)
+            segedlista=[]
+            beosztas={}
+
+"""
+2. feladat
+Hány bejegyzés található az állományban? Az eredményt írassa ki a képernyőre! 
+"""
+print("2. feladat")
+print(f"A fájlban {len(beosztasok)} bejegyzés van.")
+            
